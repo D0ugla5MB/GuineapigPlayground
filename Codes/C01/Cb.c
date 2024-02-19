7abaf5f49be1a15b8168d8bd81a5825f0171b73c
DMB <douglasmbdev@gmail.com>
2024-02-19 11:53:09 -0300

    conflicts fixed (?)

--- Codes/C01/Cb.c
+++ Codes/C01/Cb.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include <time.h>
-#include "Cb.h"
-=======
-#include <limits.h>
-#include <float.h>
 #include <time.h>
 
 #define MY_SIZEOF(arr) ((char *)(&arr + 1) - (char *)(&arr))
-
-const char SEPARATOR[] = "\n----------------------------------\n\0";
-
-void put_space()
-{
-    printf("%s", SEPARATOR);
-    printf("%s", SEPARATOR);
-    printf("%s", SEPARATOR);
-}
 
 void clear_screen()
 {
     printf("\033[2J\033[H");
 }
->>>>>>> ref
 
 char random_char()
 {
@@ -32,8 +17,7 @@ char random_char()
 
 int random_number(int min, int max)
 {
-<<<<<<< HEAD
-    srand(time(NULL));
+    // srand(time(NULL)); the output will be the same IF the calls would happen in the same second
     return min + rand() % (max - min + 1);
 }
 
@@ -52,23 +36,6 @@ int getNum(int num)
     return num;
 }
 
-=======
-    // srand(time(NULL)); the output will be the same IF the calls would happen in the same second
-    return min + rand() % (max - min + 1);
-}
-
->>>>>>> ref
-void fillArray(int arr[], int arrSize)
-{
-    // int size = sizeof(arr)/sizeof(arr[0]);
-
-    for (int i = 0; i < arrSize; i++)
-    {
-<<<<<<< HEAD
-        arr[i] = random_number(-10000, 10000);
-    }
-}
-
 void showArray(int arr[], int arrSize)
 {
     for (int i = 0; i < arrSize; i++)
@@ -83,28 +50,10 @@ int *getLastAddress(int arr[], int arrSize)
     int numElem = sizeof(arrSize) / sizeof(arr[0]);
     return &(arr[numElem - 1]);
 }
-=======
-        arr[i] = i;
-    }
-}
 
-int getArrSize(int arr[]){
-    return sizeof(arr)/sizeof(int);
-}
-
-void showInfo(int arr[], int arrP[], int size)
+int getArrSize(int arr[])
 {
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d:\tad:\020%p\tval:\020%d\n", i, &arr[i], arr[i]);
-    }
-    put_space();
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d:\tad:\020%p\tval:\020%d\n", i, &arrP[i], arrP[i]);
-    }
-
-    printf("\n");
+    return sizeof(arr) / sizeof(int);
 }
 
 // int arrSize = sizeof(arr) / sizeof(int); // dont work witch: parameters, poiters, matrix
@@ -114,14 +63,13 @@ int main(int argc, char const *argv[])
     srand(time(NULL));
     put_space();
 
-    int arr[] = {1,2,3,4,5,6,7,8,9,0};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
     int arrA[100];
-    fillArray(arrA, 100); 
-    int* p = arrA;
+    fillArray(arrA, 100);
+    int *p = arrA;
 
-    printf("%p\t%p\n", p, p+1);
+    printf("%p\t%p\n", p, p + 1);
     printf("%p\t%p\n", &arrA[0], &arrA[0] + 8);
 
     return 0;
 }
->>>>>>> ref
