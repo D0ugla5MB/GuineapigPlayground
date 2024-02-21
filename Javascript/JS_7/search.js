68ca0b82a82444a0abf02480a1be21a58d09b8e7
DMB <douglasmbdev@gmail.com>
2024-02-20 23:00:23 -0300

    JS_7: issues on 'const' function expression

--- Javascript/JS_7/search.js
+++ Javascript/JS_7/search.js
@@ -1,22 +1,42 @@
-import { getParameters } from "./search_parameters.js";
+import { getParamListSize, getParameters } from "./search_parameters.js";
 
 const apiUrl = 'https://api.waifu.im/search';  // Replace with the actual API endpoint URL
 const params = (parameters) => {
-    const paramProp  = Object.entries(parameters);
-    
+    const paramProp = Object.entries(parameters);
+    const cntDeletedProp = validURL();
+    let aux = 0;
     for (const [prop, value] of paramProp) {
-        if(value == null || value == ""){
+        if (value == null || value == "" || value == false) {
+            cntDeletedProp();
+            ++aux;
             delete parameters[prop];
         }
     }
-    console.log(parameters);
-    return parameters;
+    return { parameters, aux };
 }
 
-const queryParams = new URLSearchParams(params(getParameters()));
+
+const queryParams = new URLSearchParams(params(getParameters()).parameters);
 const requestUrl = `${apiUrl}?${queryParams}`;
 
+function validURL() {
+    let cnt = -1;
+
+    function count() {
+        cnt++;
+    }
+
+    return count;
+}
+
+
 export async function getSearchData() {
+    const objectLength = getParamListSize();
+    const counter = window.params("").aux;
+    console.log(counter);
+    if (counter >= objectLength || counter == undefined) {
+        throw new Error("We haven't those images :/");
+    }
 
     return await fetch(requestUrl)
         .then(response => {
--- Javascript/JS_7/search_parameters.js
+++ Javascript/JS_7/search_parameters.js
@@ -3,21 +3,28 @@ import { getTags } from "./tags.js";
 const tagsList = await getTags();
 
 const parameter_list = {
-    included_tags: tagsList["versatile"][0],  //array str
-    excluded_tags: null,  //array str
+    included_tags: "",  //array str
+    excluded_tags: "",  //array str
     included_files: null, //array str
     excluded_files: null, //array str
     is_nsfw: "",        //str
-    gif: null,            //boolean
+    gif: false,            //boolean
     order_by: "",       //str
     orientation: "",    //str
-    many: null,           //boolean
+    many: false,           //boolean
     full: null,           //boolean
     width: "",          //str
     height: "",         //str
     byte_size: ""       //str
 };
 
+export function getParamListSize(){
+    return 12;
+}
+
+function cntNumProperties(){
+    return Object.values(parameter_list).length; //need being fixed
+}
 
 export function getParameters() {
     return parameter_list;
