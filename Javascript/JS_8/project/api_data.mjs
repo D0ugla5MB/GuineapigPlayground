7dd7151fe07653a06835b6d1281d5a18d49aa75b
DMB <douglasmbdev@gmail.com>
2024-03-01 23:59:26 -0300

    more playing with NodeJS plus npm

--- Javascript/JS_8/project/api_data.mjs
+++
@@ -1,9 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export function getRandomData() { return makeRdnData(); }
-
-function makeRdnData() {
-    const randomName = faker.person.fullName();
-    const randomEmail = faker.internet.email();
-    return { randomName, randomEmail };
-}
