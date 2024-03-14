f6d7ee99890a91770a0952eb7fa3829505b4f172
DMB <douglasmbdev@gmail.com>
2024-03-14 00:06:19 -0300

    more React playing

--- React/R1/project/src/index.js
+++ React/R1/project/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
-import { faker } from '@faker-js/faker';
+import { faker, ro } from '@faker-js/faker';
+import { useState } from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 function makePerson() {
     return { firstName: faker.person.firstName(), lastName: faker.person.lastName() };
@@ -11,6 +15,7 @@ function makePerson() {
 function Title() {
     return <h1>These are my components!</h1>
 }
+
 
 function Person() {
 
@@ -22,22 +27,62 @@ function Person() {
         </li>
     );
 }
+function MyButton() {
+    const [newPerson, setNewPerson] = useState(makePerson());
+    /* 
+    (global)var newPerson = makePerson();
+    function setNewPerson(p){
+        newPerson = p;
+    }
+
+    function addNewPerson() {
+        setNewPerson(makePerson());
+        return newPerson;
+    }
+    
+     */
+    function addNewPerson() {
+        setNewPerson(makePerson());
+    }
+    return (
+        <>
+            <button onClick={addNewPerson}>
+                Click me
+            </button>
+            <p className='space'>{`${newPerson.firstName}\t${newPerson.lastName}`}</p> 
+        </>
+    );
+}
 
 function PeopleList() {
     const people = [];
-    let stackQty = Math.round((1 + Math.random()) * 10);
+    let stackQty = 5;
 
-    while (stackQty >= 0) {
+    while (stackQty > 0) {
         people.push(makePerson());
         stackQty--;
     }
 
     return (
-        <ul>
-            {people.map((person) => (
-                <Person key={crypto.randomUUID()} person={person} />
-            ))}
-        </ul>
+        /*{
+                (function () {
+                    const aux = [];
+                    for (const perosn of people) {
+                        aux.push(<Person key={crypto.randomUUID()} person={people[perosn]} />);
+                    }
+                    return aux;
+                })()
+            }*/
+
+        <>
+            <ul>
+                {
+                    people.map((person) => (
+                        <Person key={crypto.randomUUID()} person={person} />
+                    ))
+                }
+            </ul>
+        </>
     );
 }
 
@@ -46,10 +91,9 @@ function Content() {
         <>
             <Title />
             <br></br>
-            <PeopleList />
+            <MyButton />
         </>
     );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(<Button />);
+root.render(<Content />);
